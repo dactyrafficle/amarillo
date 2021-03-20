9f52f0bc5024f27210211c710afa19650473ff23
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-03-20 19:08:35 -0400

    Add files via upload

--- treemap_boxes_from_treemap_obj.js
+++ treemap_boxes_from_treemap_obj.js
@@ -1,8 +1,30 @@
 
 // look at box, is it greater in height than width?
 
-function returnArrayOfTreeMapBoxesFromTreemapObject(x, y, w, h, treemapObj) {
 
- console.log(treemapObj);
+// i wonder if i can make it relative
+function returnArrayOfTreeMapBoxesFromTreemapObject(x, y, w, h, obj) {
+
+ console.log(obj);
+ 
+ let count = obj.x_count;
+ let value = obj.x_value;
+ 
+ // TURN THE GROUPS INTO AN ARRAY OF OBJECTS, WITH DATA FROM THE PARENTS
+ let arr = [];
+ Object.keys(obj.groups).forEach(function(a,b,c) {
+   obj.groups[a].x_count_parent = count;
+   obj.groups[a].x_value_parent = value;
+   arr.push(obj.groups[a]);
+ });
+ console.log(arr);
+ 
+ // SORT ON THE BASIS OF x_value
+ arr.sort(function(a,b) {
+   if (a.x_value > b.x_value) {return -1;};
+   if (a.x_value < b.x_value) {return 1;};
+   if (a.x_value === b.x_value) {return 0;};
+ });
+ console.log(arr);
 
 };
