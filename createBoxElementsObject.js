61d9fe40b30ccf7dc12558d21ed5ce3599c813e3
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-04-18 04:48:24 -0400

    Add files via upload

--- createBoxElementsObject.js
+++ createBoxElementsObject.js
@@ -1,8 +1,49 @@
-function createBoxElementsObject(arr, s, s_unit) {
 
+
+function drawBoxes(b) {
+  
+  // an array of object
   let boxes = document.createElement('div');
   boxes.id = 'box-container';
   boxes.style.position = 'relative';
+  
+  let A = 0;
+  
+  for (let i = 0; i < b.boxes.length; i++) {
+    let box = b.boxes[i].el
+    boxes.appendChild(box);   
+  }
+ 
+  boxes.style.width = b.container.w;
+  boxes.style.height = b.container.h;
+  return boxes;
+}
+
+function addEventListnersAndCreateInfoBox(b) {
+  let div = document.createElement('div');
+  div.id = 'hank';
+  
+  for (let i = 0; i < b.boxes.length; i++) {
+    console.log(b.boxes[i].obj);
+    let el = b.boxes[i].el;
+    el.addEventListener('click', function() {
+      console.log(this);
+      div.innerHTML = '';
+      div.innerHTML += '<p> PARENT: ' + b.boxes[i].obj.parent + '</p>';
+      div.innerHTML += '<p> NAME: ' + b.boxes[i].obj.name + '</p>';
+      div.innerHTML += '<p> VALUE: ' + b.boxes[i].obj.value + '</p>';
+    });
+    
+  }
+  
+  
+  return div;
+}
+
+function createArrOfBoxElementObjects(arr, s, s_unit) {
+
+  let output = [];
+
   
   let A = 0;
 
@@ -10,28 +51,45 @@ function createBoxElementsObject(arr, s, s_unit) {
     let obj = arr[i];
     let box = document.createElement('div');
     box.classList.add('box');
+    box.style.position = 'absolute';
+    box.style.border = '1px solid #999';
+    
     box.style.top = (obj.py)/100*s + s_unit;
     box.style.left = obj.px/100*s + s_unit;
     
     let w = obj.sw/100*s;
     let h = obj.sh/100*s;
     A += w*h;
+    obj.A = A;
     
     box.style.width = w + s_unit;
     box.style.height = h + s_unit;
-    box.style.border = '1px solid #999';
-    boxes.appendChild(box);
+    
+    output.push({
+      'el': box,
+      'obj': obj
+     });
   }
   
   //console.log(boxes);
   //console.log(Math.sqrt(A));
   
-  boxes.style.width = Math.sqrt(A) + s_unit;
-  boxes.style.height = Math.sqrt(A) + s_unit;
+  //boxes.style.width = Math.sqrt(A) + s_unit;
+  //boxes.style.height = Math.sqrt(A) + s_unit;
   
+  return {
+    "container":{    
+      "w": Math.sqrt(A) + s_unit,
+      "h": Math.sqrt(A) + s_unit
+     },
+    "boxes": output
+  };
+  
+  /*
   return {
     'boxes': boxes,
     'width': Math.sqrt(A),
     'height': Math.sqrt(A)
   }
+  */
 }
--- setup.js
+++ setup.js
@@ -4,14 +4,15 @@
 let data = fetch('data.json?x=' + Math.random()).then(r => r.json()).then(arr => {
   
   let x = arrOfObj2PivotPartA(arr, 'val', 'cat1', 'cat2', 'cat3', 'cat4');
+  //let x = arrOfObj2PivotPartA(arr, 'val', 'cat1', 'cat2', 'cat3');
   let y = arrOfObj2PivotPartB(x);
   let z = createBoxObjects(0, 0, 100, 100, y.children); // remove the first 4 arguments
-  let b = createBoxElementsObject(z, 700, 'px');
-  
-  console.log(b);
+  let b = createArrOfBoxElementObjects(z, 700, 'px'); // good
 
-  let boxesContainer = document.getElementById('container');
-  boxesContainer.appendChild(b.boxes);
+
+  document.getElementById('container').appendChild(drawBoxes(b));
+  document.getElementById('infobox').appendChild(addEventListnersAndCreateInfoBox(b));
+
   
 });
 
