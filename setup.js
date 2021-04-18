a7d6870b3168f931735e9f87d9a3499746390acb
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-04-18 06:13:32 -0400

    Add files via upload

--- setup.js
+++ setup.js
@@ -4,9 +4,11 @@
 let data = fetch('data.json?x=' + Math.random()).then(r => r.json()).then(arr => {
   
   let x = arrOfObj2PivotPartA(arr, 'val', 'cat1', 'cat2', 'cat3', 'cat4');
-  //let x = arrOfObj2PivotPartA(arr, 'val', 'cat1', 'cat2', 'cat3');
-  let y = arrOfObj2PivotPartB(x);
-  let z = createBoxObjects(0, 0, 100, 100, y.children); // remove the first 4 arguments
+  let y = arrOfObj2PivotPartB(x);  // i need to add levels here, the lineage
+  console.log(y);
+  let y2 = addAncestors(y, ['']);
+  
+  let z = createBoxObjects(0, 0, 100, 100, y2.children); // remove the first 4 arguments
   let b = createArrOfBoxElementObjects(z, 700, 'px'); // good
 
 
